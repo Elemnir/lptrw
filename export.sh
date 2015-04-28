a66b5f11ea2836b18c9754e28a91de56a1954de8
Elemnir <ahoward0920@gmail.com>
2015-04-27 22:20:09 -0400

    Fixed typos in excerise 2

--- export.sh
+++ export.sh
@@ -2,6 +2,6 @@
 
 LPTRW_ROOT=/home/karkadan/docs/lptrw
 
-source LPTRW_ROOT/venv/bin/activate
+source $LPTRW_ROOT/venv/bin/activate
 grip --export $LPTRW_ROOT/src/ex$1.md
 mv $LPTRW_ROOT/src/ex$1.html $LPTRW_ROOT/html/.
